2d5b3849e1fdf2ecb54cfcc6eada1da4c274eca6
cph-dr147 <dana.albidani@gmail.com>
2025-12-12 16:01:19 +0100

    Changes by zainabalmosawi

--- src/main/java/com/example/gossipgirl_/HelloApplication.java
+++ src/main/java/com/example/gossipgirl_/HelloApplication.java
@@ -10,14 +10,18 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        // ✅ Connect to SQLite at startup
+        DBConnector.getConnection();
+        System.out.println(new java.io.File("console.sql").getAbsolutePath());
 
+        // Load FXML and create scene
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
 
+        // Use your ViewSwitcher logic
         ViewSwitcher.setScene(scene);
         ViewSwitcher.switchTo(View.START);
-
 
         stage.setScene(scene);
         stage.show();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.gossipgirl_ {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.sql;
 
     opens com.example.gossipgirl_ to javafx.fxml;
     exports com.example.gossipgirl_;
